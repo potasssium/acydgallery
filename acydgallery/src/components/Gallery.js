2f790384cbaea47170ac6b9265c993483e574f40
Kay <kaydenceaubin@gmail.com>
2025-03-09 14:47:09 -0700

    icky

--- acydgallery/src/components/Gallery.js
+++ acydgallery/src/components/Gallery.js
@@ -8,20 +8,11 @@ const Gallery = () => {
                 <div className='box' id='box-1'>
                     <img src={'/imgs/place1.jpg'}></img>
                 </div>
-                <div className='overlay' id='over1'>
-                    <div className='img-text'>meoq</div>
-                </div>
                 <div className='box' id='box-2'>
                     <img src={'/imgs/place2.jpg'}></img>
                 </div>
-                <div className='overlay' id='over2'>
-                    <div className='img-text2'>meoq</div>
-                </div>
                 <div className='box' id='box-3'>
                     <img src={'/imgs/place3.jpg'}></img>
-                </div>
-                <div className='overlay' id='over3'>
-                    <div className='img-text3'>meoq</div>
                 </div>
             </div>
         </section>
